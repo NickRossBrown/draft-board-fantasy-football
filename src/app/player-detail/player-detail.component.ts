b3ed1c2dfb85a70d2b6f323887cc2bf752b350fa
Nick Brown <nickrossbrown@gmail.com>
2018-08-17 14:42:36 -0700

    add playerToDisplay method in playerServices. Also add params,location,playService to player-detail componenet to retreive player information

--- src/app/player-detail/player-detail.component.ts
+++ src/app/player-detail/player-detail.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+import { Player } from '../player.model';
+import { PlayerService } from '../player.service';
 
 @Component({
   selector: 'app-player-detail',
   templateUrl: './player-detail.component.html',
-  styleUrls: ['./player-detail.component.css']
+  styleUrls: ['./player-detail.component.css'],
+  providers: [PlayerService]
 })
 export class PlayerDetailComponent implements OnInit {
+  PlayerID: number;
+  PlayerToDisplay: Player;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute, private location: Location,
+    // By including the parameters constructor(private route: ActivatedRoute, private location: Location), any instance of PlayerDetailComponent will have route and location properties that can be accessed by calling this.route and this.location.
+    private playerService: PlayerService
+  ) { }
 
   ngOnInit() {
+    this.route.params.forEach((urlParameters) => {
+     this.playerId = parseInt(urlParameters['id']);
+     console.log(this.playerId)
+   });
+    this.playerToDisplay = this.playerService.getPlayerById(this.playerId);
+    console.log(this.playerToDisplay)
   }
 
 }
