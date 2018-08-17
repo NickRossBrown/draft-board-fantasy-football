a3e34f75c4599aa06b80cbc6ef56a2c2ccf4fc9d
Nick Brown <nickrossbrown@gmail.com>
2018-08-17 13:45:22 -0700

    add mock-players in mock-plauer.ts file. Add players constructor in player.model.ts file. Also add players services that should link the mock-players to the draftBoard conponent

--- src/app/draft-board/draft-board.component.ts
+++ src/app/draft-board/draft-board.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { PlayerService } from '../player.service';
+
 
 @Component({
   selector: 'app-draft-board',
   templateUrl: './draft-board.component.html',
   styleUrls: ['./draft-board.component.css']
+  providers: [PlayerService]
 })
 export class DraftBoardComponent implements OnInit {
+  players: PLayer[];
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private playerService: PlayerService
+  ) {}
+  // this.playerService should anywhere in the draftbaord Component class.
 
-  ngOnInit() {
+  ngOnInit(){
+    this.players = this.playerService.getPlayers();
   }
 
 }
---
+++ src/app/player.model.ts
@@ -0,0 +1,5 @@
+export class Player {
+
+  constructor (public name: string, public position: string, public ranking: number, public id: number) { }
+  
+}
